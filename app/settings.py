47412372b1420bf621fd6dbc4bd56943dd03dac7
patresio <patresio@gmail.com>
2024-07-13 16:16:06 -0300

    adjusting settings

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party apps
+    "rest_framework",
+    # My APPs
     "genres",
 ]
 
--- genres/urls.py
+++ genres/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from genres.views import genre_create_list_view, genre_detail_view
 
+app_name = "genres"
+
 urlpatterns = [
-    path("", genre_create_list_view, name="genre-create-list"),
-    path("<int:pk>/", genre_detail_view, name="genre-detail-view"),
+    path("", genre_create_list_view, name="create-list"),
+    path("<int:pk>/", genre_detail_view, name="detail-view"),
 ]
