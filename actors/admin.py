c77d2dcf78c6480d967a4314f48087de6ba00990
Ricardo Pereira <patresio@gmail.com>
2024-08-02 09:15:37 -0300

    admins for django admin adjusting

--- actors/admin.py
+++ actors/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from actors.models import Actor
+
 # Register your models here.
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "birthday", "nationality"]
--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from movies.models import Movie
+
 # Register your models here.
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "release_date", "genre"]
